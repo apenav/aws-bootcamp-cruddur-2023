6ed28265bf5363894a0987451c897a9dbb4aedf4
apenav <apenav@users.noreply.github.com>
2023-02-25 17:26:09 +0100

    week1 (#2)

---
+++ backend-flask/run_local.sh
@@ -0,0 +1,2 @@
+echo "Browse to http://localhost:4567/api/activities/home to test"
+docker run --rm -p 4567:4567 --name backend-flask apenav/aws-bootcamp-cruddur-2023-backend-flask:latest
---
+++ frontend-react-js/build_local.sh
@@ -0,0 +1,2 @@
+docker rmi apenav/aws-bootcamp-cruddur-2023-frontend-react-js:latest
+docker build -t apenav/aws-bootcamp-cruddur-2023-frontend-react-js:latest .
