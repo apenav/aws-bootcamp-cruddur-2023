def06065cb803858737f3f5cb1d06560db02b806
apenav <apenav@users.noreply.github.com>
2023-07-14 08:50:55 +0200

    week4 (#5)

--- backend-flask/services/show_activity.py
+++ backend-flask/services/show_activity.py
@@ -1,27 +1,18 @@
 from datetime import datetime, timedelta, timezone
-from aws_xray_sdk.core import xray_recorder
-
 class ShowActivities:
   def run(activity_uuid):
-    logger.info("show activities")
-    with tracer.start_as_current_span("show-activities-mock-data"):
-      try:
-        now = datetime.now(timezone.utc).astimezone()
-        results = [{
-          'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-          'handle':  'Andrew Brown',
-          'message': 'Cloud is fun!',
-          'created_at': (now - timedelta(days=2)).isoformat(),
-          'expires_at': (now + timedelta(days=5)).isoformat(),
-          'replies': {
-            'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
-            'handle':  'Worf',
-            'message': 'This post has no honor!',
-            'created_at': (now - timedelta(days=2)).isoformat()
-          }
-        }]
-      finally:  
-      # Close the segment
-        xray_recorder.end_subsegment()
-    span.set_attribute("app.result_length", len(results)) 
+    now = datetime.now(timezone.utc).astimezone()
+    results = [{
+      'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
+      'handle':  'Andrew Brown',
+      'message': 'Cloud is fun!',
+      'created_at': (now - timedelta(days=2)).isoformat(),
+      'expires_at': (now + timedelta(days=5)).isoformat(),
+      'replies': {
+        'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
+        'handle':  'Worf',
+        'message': 'This post has no honor!',
+        'created_at': (now - timedelta(days=2)).isoformat()
+      }
+    }]
     return results
